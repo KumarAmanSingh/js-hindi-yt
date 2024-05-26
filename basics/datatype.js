bb890dd94a967a49c038feff4024c00611535211
Aman Kumar <99633913+KumarAmanSingh@users.noreply.github.com>
2024-05-26 05:05:52 +0000

    datatypes

---
+++ basics/datatype.js
@@ -0,0 +1,16 @@
+"use strict"; //treats all js codes as newer version
+//alert(3+3) //we are using node js not browser,in browser js is inbuilt...
+let name = "Aman"
+let age = 20
+let isLoggedIn = true
+
+//name is String datatype
+//age is integer datatype
+//loggedIn is boolean datatype
+
+console.log(typeof(name));
+console.log(typeof(age));   
+console.log(typeof(isLoggedIn));
+
+console.log(typeof(null));      //datatype of null is object
+console.log(typeof(undefined)); //datatype of undefined is undefined
